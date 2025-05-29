cf85a0058876a16a0f192acd69b60843e724bcb1
yesnapoleondynamite <yesnapoleondynamite@gmail.com>
2025-05-28 23:10:27 -0500

    CF_DU

--- src/js/json.js
+++ src/js/json.js
@@ -161,7 +161,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/228136_CF04_DU.pdf',
+        download: 'downloads/228117_CF06_DU.pdf',
       },
       {
         icono: 'fas fa-download',
